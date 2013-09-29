e66490bc3bb50dcc134f9496d3713ebcdbeb706a
Robert Stoll <rstoll@tutteli.ch>
2013-09-29 22:47:06 +0200

    TSPHP-601 getFirstUseDefinitionAst in NamespaceScope not case insensitive - fixed, improved naming changed getUse -> uses now case sensitive version

--- src/ch/tutteli/tsphp/typechecker/scopes/INamespaceScope.java
+++ src/ch/tutteli/tsphp/typechecker/scopes/INamespaceScope.java
@@ -15,9 +15,14 @@ public interface INamespaceScope extends ICaseInsensitiveScope
      * Return one or more AST which contains the use declaration for the alias or null if the alias could not be found.
      *
      * @param alias The alias which shall be found
-     * @return A list of ASTs or null
+     * @return A list of symbols or null
      */
     List<IAliasSymbol> getUse(String alias);
 
-    ITSPHPAst getFirstUseDefinitionAst(String alias);
+    /**
+     * Return the corresponding definition ast of the first definition found for the given {@code alias} ignoring case.
+     * @param alias The name of the alias which shall be found
+     * @return The definition ast or null if the alias wasn't found
+     */
+    ITSPHPAst getCaseInsensitiveFirstUseDefinitionAst(String alias);
 }
