73077f94e89f6a9bec2b07163c1d3ea6663fafa7
Robert Stoll <rstoll@tutteli.ch>
2014-06-30 20:16:43 +0200

    - TSPHP-651 NullPointer when type for variable is missing -- forgot to add the test in the last commit

---
+++ test/ch/tsphp/typechecker/test/integration/testutils/typecheck/ATypeCheckWithReferenceErrorTest.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the TSPHP project published under the Apache License 2.0
+ * For the full copyright and license information, please have a look at LICENSE in the
+ * root folder or visit the project's website http://tsphp.ch/wiki/display/TSPHP/License
+ */
+
+package ch.tsphp.typechecker.test.integration.testutils.typecheck;
+
+import org.junit.Ignore;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@Ignore
+public abstract class ATypeCheckWithReferenceErrorTest extends ATypeCheckTest
+{
+    public ATypeCheckWithReferenceErrorTest(String testString) {
+        super(testString);
+    }
+
+    @Override
+    protected void checkErrors() {
+        verifyTypeCheck();
+    }
+
+    @Override
+    protected void checkReferences() {
+        assertTrue(testString + " failed. Exceptions expected but nothing was thrown." + exceptions,
+                typeCheckErrorReporter.hasFoundError());
+        assertFalse(testString + " failed. reference walker exceptions occurred", reference.hasFoundError());
+
+        verifyReferences();
+    }
+}
---
+++ test/ch/tsphp/typechecker/test/integration/typecheck/AssignmentOperatorWithReferenceErrorTest.java
@@ -0,0 +1,71 @@
+/*
+ * This file is part of the TSPHP project published under the Apache License 2.0
+ * For the full copyright and license information, please have a look at LICENSE in the
+ * root folder or visit the project's website http://tsphp.ch/wiki/display/TSPHP/License
+ */
+
+package ch.tsphp.typechecker.test.integration.typecheck;
+
+import ch.tsphp.common.ITSPHPAst;
+import ch.tsphp.typechecker.error.ITypeCheckerErrorReporter;
+import ch.tsphp.typechecker.test.integration.testutils.typecheck.ATypeCheckWithReferenceErrorTest;
+import org.antlr.runtime.RecognitionException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.exceptions.base.MockitoAssertionError;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+
+public class AssignmentOperatorWithReferenceErrorTest extends ATypeCheckWithReferenceErrorTest
+{
+
+    public AssignmentOperatorWithReferenceErrorTest() {
+        super("");
+    }
+
+    @Override
+    protected void verifyTypeCheck() {
+        // nothing to check in addition.
+        // No error other than the reference error should have occurred.
+        // That's already checked in ATypeCheckWithReferenceErrorTest
+    }
+
+    //see TSPHP-785 - forward reference should not cause type checker exception (only reference exception)
+    @Test
+    public void checkAssignment_ForwardReference_OnlyReferenceException() throws RecognitionException {
+        testString = "$s = 'hello'; string $s;";
+        check();
+        try {
+            verify(typeCheckErrorReporter).forwardReference(any(ITSPHPAst.class), any(ITSPHPAst.class));
+        } catch (MockitoAssertionError e) {
+            System.err.println(testString + " failed.");
+            throw e;
+        }
+    }
+
+    //see TSPHP-785 - not defined variable should not cause type checker exception (only reference exception)
+    @Test
+    public void checkAssignment_NotDefined_OnlyReferenceException() throws RecognitionException {
+        testString = "$s = 'hello';";
+        check();
+        try {
+            verify(typeCheckErrorReporter).notDefined(any(ITSPHPAst.class));
+        } catch (MockitoAssertionError e) {
+            System.err.println(testString + " failed.");
+            throw e;
+        }
+    }
+
+    @Override
+    protected ITypeCheckerErrorReporter createTypeCheckErrorReporter() {
+        return spy(super.createTypeCheckErrorReporter());
+    }
+}
+
